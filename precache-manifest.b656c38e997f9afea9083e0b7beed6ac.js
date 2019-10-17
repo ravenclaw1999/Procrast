b332074df1f052f69e2afa51db4cfa37e45e70b6
EC2 Default User <ec2-user@ip-172-31-17-5.us-east-2.compute.internal>
2019-10-17 04:32:04 +0000

    Updates

---
+++ precache-manifest.b656c38e997f9afea9083e0b7beed6ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e48525963c6b1bd8828245f506c110cb",
+    "url": "/Procrast/index.html"
+  },
+  {
+    "revision": "238a8b1fd84d59a0cb3b",
+    "url": "/Procrast/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "ff323b688b5079004163",
+    "url": "/Procrast/static/css/main.15905ee8.chunk.css"
+  },
+  {
+    "revision": "238a8b1fd84d59a0cb3b",
+    "url": "/Procrast/static/js/2.bb6f919c.chunk.js"
+  },
+  {
+    "revision": "ff323b688b5079004163",
+    "url": "/Procrast/static/js/main.b23f05a8.chunk.js"
+  },
+  {
+    "revision": "e872f37a3aafd5c3147a",
+    "url": "/Procrast/static/js/runtime~main.edd8e6d0.js"
+  }
+]);
