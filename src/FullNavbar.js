4944db099964d10ee15fe7d6b6a013e175fa721a
ravenclaw1999 <702schowdhury@gmail.com>
2021-04-01 21:57:12 -0400

    updated logo

--- src/FullNavbar.js
+++ src/FullNavbar.js
@@ -7,7 +7,7 @@ import {
 export default function FullNavbar(props) {
     return (
       <Navbar bg="light" expand="lg">
-       <img className="img" src="https://www.freelogodesign.org/file/app/client/thumb/69b4dfc3-aae5-4ae9-add2-ae9eb07640a4_200x200.png?1601872696321" />
+       <img className="img" src="public/site_logo.png" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
           </Nav>
